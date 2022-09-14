8ba4b74277491c0c7370332d83587cd0b6e3ca63
Max Guerrero <maxguerrero94@gmail.com>
2022-09-13 21:45:57 -0700

    verifying commit works

--- hash_table_driver.c
+++ hash_table_driver.c
@@ -1,5 +1,11 @@
 #include "hash_table_driver.h"
 
+
+int buildWordTable(tableSlot_t *slot, uint32_t size)
+{
+
+}
+
 void createWordData(wordData_t * wordData, const char *wordStr, 
 		    uint32_t decade, uint32_t count)
 {
--- main-driver.c
+++ main-driver.c
@@ -3,6 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-
+  // main
   return 0;	
 }
